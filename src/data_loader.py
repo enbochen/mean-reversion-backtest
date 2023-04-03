a26bb81c45bd672f36e370f11fe389a940613cc9
Enbo Chen <enbo.chen.ai@gmail.com>
2023-04-03 12:22:03 +0300

    update the comments

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,18 +2,33 @@ import pandas as pd
 
 
 def load_data(file_path):
-    # Load data
+    """
+    Load data from a CSV file.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        data (pandas.DataFrame): The loaded data as a pandas DataFrame.
+    """
     data = pd.read_csv(file_path, index_col='timestamp', parse_dates=True)
     data.index = pd.to_datetime(data.index, unit='ms')
     return data
 
 
 def clean_data(data):
-    # Print data information
+    """
+    Clean data by removing duplicates and rows with missing data, and converting column data types.
+
+    Args:
+        data (pandas.DataFrame): The raw data to be cleaned.
+
+    Returns:
+        data (pandas.DataFrame): The cleaned data as a pandas DataFrame.
+    """
     print(data.info())
 
     # Remove any duplicate rows
-    print(data[data.duplicated()])
     data = data[~data.index.duplicated()]
 
     # Remove any rows with missing data
@@ -27,5 +42,8 @@ def clean_data(data):
         'close': 'float',
         'vol': 'float'
     })
+
+    # Validate that float values are greater than 0
+    data = data[data.apply(lambda x: x > 0)]
 
     return data
